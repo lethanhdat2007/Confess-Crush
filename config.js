b18e405e1e4ca7b5bd98e24b403b81968650960f
lethanhdat2007 <94958583+lethanhdat2007@users.noreply.github.com>
2022-01-10 14:51:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 const CONFIG = {
-    titleWeb: "c ơi em phái c",
-    introTitle: 'Chị Phương Uyên oiiii',
+    titleWeb: "cảm nhận về Sang",
+    introTitle: 'Sang oiiii',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao tui cứ đi nhầm đường
+    Lạc vào tim Sang lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    title: 'Phải chăng tui đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng tui say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'sang thích Đạt',
+    btnNo: 'sang thích Nghi',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
