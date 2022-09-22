5da70c2bc382af5e692b8c12ba12c290d2027c30
lethanhdat2007 <94958583+lethanhdat2007@users.noreply.github.com>
2022-09-22 02:21:42 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    titleWeb: "iu tuuyn nhứt",
-    introTitle: 'Tuuyn oiiii',
+    titleWeb: "Đạt muốn nóii",
+    introTitle: 'Mii oiiii',
     introDesc: `Trái đất vốn lạ thường
     Mà sao anh lại đi nhầm đường
     Lạc vào tim em lẻ loi`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Em có thể mở lòng từ bi iu anh hog ',
+    title: 'Phải chăng Đạt đã yêu Mi ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Mi iu Đạt nhaaaa ',
     btnYes: 'được chứ',
     btnNo: 'hong được ròi',
     question: 'Trên thế giới hơn 7 tỉ người mà sao anh lại gặp em <3',
     btnReply: 'Có iu tui hog',
     reply: 'iu bạn vlozz <33333333',
-    mess: 'anh biết mà 🥰. Yêu em nhiều nhiều  😘😘',
-    messDesc: 'cứi anh nhee em <33333',
+    mess: 'Đạt biết mà 🥰. Yêu Mi nhiều nhiều  😘😘',
+    messDesc: 'Hôm nào gảnh hai đứa mình đi chụp hình nhaaaaaaaaa...',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/lethanhdat/Confess-Crush'
 }
