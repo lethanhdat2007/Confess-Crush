fd91ea39c137e4ed125487b048833ace412efc12
lethanhdat2007 <94958583+lethanhdat2007@users.noreply.github.com>
2024-02-21 19:22:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
     titleWeb: "Đạt muốn nóii",
-    introTitle: 'Chị oiiii',
+    introTitle: 'Châu oiiii',
     introDesc: `Trái đất vốn lạ thường
     mà sao Đạt lại đi nhầm đường
-    lạc vào tim chị lẻ loi`,
+    lạc vào tim Châu lẻ loi`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng Đạt đã yêu chị ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'chị iu Đạt nhaaaa ',
+    title: 'Phải chăng Đạt đã yêu Châu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Châu iu Đạt nhaaaa ',
     btnYes: 'được chứ',
     btnNo: 'hong được ròi',
-    question: 'chị có iu Đạt hogg',
+    question: 'Châu có iu Đạt hogg',
     btnReply: 'gửi cho Đạt',
     reply: 'iu Dat lắm <3',
-    mess: 'Đạt biết mà 🥰. Yêu chị nhiều nhiều  😘😘',
+    mess: 'Đạt biết mà 🥰. Yêu Châu nhiều nhiều  😘😘',
     messDesc: 'Hôm nào gảnh hai đứa mình đi chụp hình nhaaaaaaaaa...',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.messenger.com/t/100024960013449'
