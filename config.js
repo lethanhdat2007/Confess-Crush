1c5c8e6931be0e0bfb54b4c58a658c54df23cb1f
lethanhdat2007 <94958583+lethanhdat2007@users.noreply.github.com>
2024-10-20 21:44:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,15 @@
 const CONFIG = {
-    titleWeb: "Đạt want to say to KAULANA WONG",
+    titleWeb: "Đạt muốn nóii",
     introTitle: 'BABYYYYYYY',
-    introDesc: `Love is like the air, we can’t always see it but we know it is always there! 
-    That’s like me, you can’t always see me but
-    I’d be always there and you know I'll always love you!`,
+    introDesc: `Anh xin lỗi em vì hôm nay anh chẳng tặng cho bé một bó hoa nào cả =((( chẳng những thế hôm nay thời gian mình bên nhau còn rất ít`,
     btnIntro: '^^HiHi^^',
-    title: 'I love u in the first meet',
-    desc: 'Do you want to become one of my girlfriends ',
+    title: 'Anh xin lỗi bé Minza nhiều lắm lắm',
+    desc: 'Tha lỗi cho anh nhaaaaaaa ',
     btnYes: 'yesssss',
     btnNo: 'never',
-    question: 'what do you think about me??',
+    question: 'iu anh hoggg',
     btnReply: 'send me',
-    reply: 'Handsome, cute, clever, Kaulana love you so much',
+    reply: 'dạ cóoo',
     mess: 'I know, I know bae, I love you so much',
     messDesc: 'I want to kiss uuuuuuu',
     btnAccept: 'Okiiiiidokiiiiii <3',
