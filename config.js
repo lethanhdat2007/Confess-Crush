9508b6c1e0ebe1f8db3de76aa6a5a16f1f96d0a8
lethanhdat2007 <94958583+lethanhdat2007@users.noreply.github.com>
2025-06-08 00:30:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Chúc Mừng sinh nhật công túa Trâm Anh",
+    introTitle: 'Công Túa Trâm Anhh',
+    introDesc: `Chúc công túa Trâm Anh tuổi mới ngày càng xinh đẹppppppppp nha`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'mình lên plan iii ănnn iii chơi với Nghị hoi nàooo',
+    desc: ' đồng ý hemmm ',
     btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnNo: 'Không nha ôn thi roàiii :3',
+    question: 'mình sẽ  iii âuuu',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: 'đi theooo plan của đạttt',
+    mess: 'bé Đạt biết mà 🥰.',
+    messDesc: 'hoaii chúc thêm lần nữa. Chúc Trâm Anhhh sẽ lun gặp may mắn ở tủi mới và nhìu thành côngggggg',
+    btnAccept: 'dạaaaaaa <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
